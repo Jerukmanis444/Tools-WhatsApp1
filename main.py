d5b7f8a191a5b98231a3cb808cf38c97d243f1aa
Jerukmanis444 <166561240+Jerukmanis444@users.noreply.github.com>
2024-04-10 05:13:21 +0700

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-copyright='Obfuscated by https://pyobfuscate.com'
-class Pyobfuscate_com():
-import os,sys,time,requests,json
-abc=input("0895383933443:")
-while True:
-      print (abc)
-      time.sleep(5)

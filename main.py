d0cb65708ba18a75a194b453532577afcbe7bff6
Jerukmanis444 <166561240+Jerukmanis444@users.noreply.github.com>
2024-04-10 04:53:55 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+copyright='Obfuscated by https://pyobfuscate.com'
+class Pyobfuscate_com():
+import os,sys,time,requests,json
+abc=input("0895383933443:")
+while True:
+      print (abc)
+      time.sleep(5)
